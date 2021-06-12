51e61e9e0c5b15027644b28b133375fe9cbd4ac8
Siddharth <69121694+siddharthkhandelwal1@users.noreply.github.com>
2021-06-12 12:09:04 +0530

    Create servlet.java

---
+++ servlet.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.sql.*;
+
+
+
+public class MySQLConnect extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javademo", "root", "");
+            PreparedStatement pst = conn.prepareStatement("Select user,pass from login where user=? and pass=?");
+            pst.setString(1, user);
+            pst.setString(2, pass);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                out.println("Correct login credentials");
+            } 
+            else {
+                out.println("Incorrect login credentials");
+            }
+        } 
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
